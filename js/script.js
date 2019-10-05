dc2521baaa14e8862d5a9b9b359d5e76e7c49393
mifist <daria1992moskalets@gmail.com>
2019-10-05 23:09:55 +0300

    testing 'rest' param + 'spread' param + shallow copy

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ const res = arr
 .map( (el) => parseInt(el) )
 .filter( (num) => num%2 )
 .reduce( (max, val) => Math.max(max, val), 0 );
-
 console.log(res);
+
+// testing 'Rest' param
+function max( ... num ) {
+    console.log(num);
+}
+max(1,4,6,7,8);
+
+// Testing 'Spread' param for arrays
+const arr2 = ['1', '2', '3'];
+const arr3 = ['5', '8', '10'];
+const res2 = Math.max( ... arr2, ... arr3 );
+console.log(res2);
+
+// Testing shallow copy arrays
+const shallowCopy = [ ... arr2, ... arr3, 568 ];
+console.log(shallowCopy);
+
 
